41dc54010840074cc7dbb0c9f147f731aaf1be0a
Svetlana <svetikyss89@gmail.com>
2023-01-26 20:48:57 -0800

    Initial Commit

---
+++ BookMe/BookMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMeApp.swift
+//  BookMe
+//
+//  Created by Светлана Светикова on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BookMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
